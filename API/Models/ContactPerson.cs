e7a6247c177a9ee665471308f1de67fe6104452c
horrormankind666 <yutthaphoom.taw@mahidol.ac.th>
2023-01-03 23:24:16 +0700

    ...

--- API/Models/ContactPerson.cs
+++ API/Models/ContactPerson.cs
@@ -16,15 +16,17 @@ namespace API.Models {
         public static List<object> GetDataSource(string datas) {
             List<object> list = new List<object>();
 
-            foreach (var data in JsonConvert.DeserializeObject<dynamic>(datas)) {
-                list.Add(new {
-                    fullName = new {
-                        th = (!String.IsNullOrEmpty(data["FullName"]["TH"].ToString()) ? data["FullName"]["TH"] : data["FullName"]["EN"]),
-                        en = (!String.IsNullOrEmpty(data["FullName"]["EN"].ToString()) ? data["FullName"]["EN"] : data["FullName"]["TH"])
-                    },
-                    email = (!String.IsNullOrEmpty(data["EmailAccount"].ToString()) ? data["EmailAccount"] : String.Empty),
-                    phoneNumber = (!String.IsNullOrEmpty(data["TelephoneNO"].ToString()) ? data["TelephoneNO"] : String.Empty)
-                });
+            if (!String.IsNullOrEmpty(datas)) {
+                foreach (var data in JsonConvert.DeserializeObject<dynamic>(datas)) {
+                    list.Add(new {
+                        fullName = new {
+                            th = (!String.IsNullOrEmpty(data["FullName"]["TH"].ToString()) ? data["FullName"]["TH"] : data["FullName"]["EN"]),
+                            en = (!String.IsNullOrEmpty(data["FullName"]["EN"].ToString()) ? data["FullName"]["EN"] : data["FullName"]["TH"])
+                        },
+                        email = (!String.IsNullOrEmpty(data["EmailAccount"].ToString()) ? data["EmailAccount"] : String.Empty),
+                        phoneNumber = (!String.IsNullOrEmpty(data["TelephoneNO"].ToString()) ? data["TelephoneNO"] : String.Empty)
+                    });
+                }
             }
 
             return list;
